9658cc5d982c869e3814b38417b8b1ba58bcac62
bensznaider <bensznaider@yahoo.com.ar>
2025-08-01 09:54:35 -0300

    DB env variables added

--- db/index.js
+++ db/index.js
@@ -2,8 +2,8 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const db = new Sequelize(
-  "challenge_chat_database_96hs", // Database
-  "challenge_chat_database_96hs_user", // Username
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
